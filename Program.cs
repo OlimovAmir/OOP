e4105ee45a5019fbcfa63ba4b047cbfb1dc6acc1
olimov Amir <olimov.amir@mail.ru>
2023-12-15 10:17:33 +0300

    add enum Color

--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
 ﻿
 class Program
 {
+    enum Color
+    {
+        red,
+        green,
+        blue, 
+        alpha
+    }
 
     class Point
     {
         public int x;  // поля класса с модификатором доступа public
         public int y;
+        public Color color;
     }
     static void Main(string[] args)
     {
         Point p = new Point();
-
         p.x = 1;
         p.y = 2;
+        p.color = Color.red;
 
-        
-        Console.WriteLine($"X: {p.x} | Y: {p.y}");
+        Point p2 = new Point();
+        p2.x = 3;
+        p2.y = 4;
+        p2.color = Color.green;
+
+        Console.WriteLine($"X: {p.x} | Y: {p.y} | color: {p.color}");
+        Console.WriteLine($"X: {p2.x} | Y: {p2.y} | color: {p2.color}");
     }
 }
