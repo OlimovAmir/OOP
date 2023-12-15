e6c60d3139a4ea45ba70896650de6453b8c03f2c
olimov Amir <olimov.amir@mail.ru>
2023-12-15 10:55:27 +0300

    created class Student

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using OOP;
+
 class Program
 {
     enum Color
@@ -14,7 +16,35 @@ class Program
         public int x;  // поля класса с модификатором доступа public
         public int y;
         public Color color;
+    };
+
+    
+   
+    static Student GetStudent() {
+        OOP.Student student = new OOP.Student();
+
+
+        student.lastName = "Amir";
+        student.firstName = "Olimov";
+        student.middleName = "Muhamadaminovich";
+        student.age = 36;
+        student.Id = Guid.NewGuid();
+        student.group = Group.IT;
+
+        return student; 
     }
+
+    static void Print(Student student)
+    {
+        Console.WriteLine("info obout Student");
+        Console.WriteLine($"ID: {student.Id}");
+        Console.WriteLine($"lastName: {student.lastName}");
+        Console.WriteLine($"firstName: {student.firstName}");
+        Console.WriteLine($"middleName: {student.middleName}");
+        Console.WriteLine($"age: {student.age}");
+        Console.WriteLine($"group: {student.group}");
+    }
+
     static void Main(string[] args)
     {
         Point p = new Point();
@@ -26,8 +56,14 @@ class Program
         p2.x = 3;
         p2.y = 4;
         p2.color = Color.green;
+        
+
 
         Console.WriteLine($"X: {p.x} | Y: {p.y} | color: {p.color}");
         Console.WriteLine($"X: {p2.x} | Y: {p2.y} | color: {p2.color}");
+
+        var student = GetStudent();
+
+        Print(student);
     }
 }
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+
+    public enum Group
+    {
+        fizik,
+        Matematic,
+        Himik,
+        IT
+    }
+    public class Student
+    {
+        public Guid Id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public Group group;
+
+    }
+}
