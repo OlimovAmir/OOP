09b72fc510635ed418352e16784793db1057220a
olimov Amir <olimov.amir@mail.ru>
2023-12-15 10:05:33 +0300

    class Point

--- Program.cs
+++ Program.cs
@@ -4,11 +4,17 @@ class Program
 
     class Point
     {
-        public int x;
+        public int x;  // поля класса с модификатором доступа public
         public int y;
     }
     static void Main(string[] args)
     {
+        Point p = new Point();
 
+        p.x = 1;
+        p.y = 2;
+
+        Console.WriteLine(p.y);
+        Console.WriteLine(p.x);
     }
 }
